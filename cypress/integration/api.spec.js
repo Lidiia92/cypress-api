105fa0ae4efe47463cc22d9c8707a9cc88e19be0
Lidii <lidiiany92@gmail.com>
2020-05-21 13:16:46 -0700

    validating status code of the response

---
+++ cypress/integration/api.spec.js
@@ -0,0 +1,18 @@
+/// <reference types='Cypress'/>
+
+describe('REST API Test with Cypress', () => {
+	it('API Test - Validate Headers', () => {
+		cy.request('https://pokeapi.co/api/v2/pokemon/25').as('pokemon');
+
+		cy.get('@pokemon')
+			.its('headers')
+			.its('content-type')
+			.should('include', 'application/json; charset=utf-8');
+	});
+
+	it('API Test - Validate Status Code', () => {
+		cy.request('https://pokeapi.co/api/v2/pokemon/25').as('pokemon');
+
+		cy.get('@pokemon').its('status').should('equal', 200);
+	});
+});
